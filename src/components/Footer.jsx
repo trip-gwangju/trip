b8690fc67cb2fc7cec2d46672135d07c07e16e6f
suy0ng <yunsuyong739@naver.com>
2023-03-14 22:38:10 +0900

    edit FooterText

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/Footer.css'
 
 function Liscence(props) {
-  return <div className="FooterText">ⓒsuy0ng All rights reserved</div>;
+  return <div className="FooterText">ⓒAll rights reserved</div>;
 }
 
 export default Liscence;
