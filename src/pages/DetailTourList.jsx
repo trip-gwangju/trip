c879055c7fa049f773767fccf9ca68c4b4be3adb
suy0ng <yunsuyong739@naver.com>
2023-03-19 01:03:15 +0900

    add DetailTourList

--- src/pages/DetailTourList.jsx
+++ src/pages/DetailTourList.jsx
@@ -4,31 +4,40 @@ import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import "../styles/TourInfoBox.css";
 
-function DetailTourList({ Lists }) {
+function DetailTourList() {
   const { tour_id } = useParams();
-  const API_KEY ="YAHkF40pRCYGFbXP%2FXeCrB3nJ4B9G9DObwb71txY0kHNQ2bqTcpLBbUDW0wrx%2BQHAYSJiDg8uYoFvsSWKYl3Kw%3D%3D";
-  const [lists, setLists] = useState([]);
+  const API_KEY =
+    "YAHkF40pRCYGFbXP%2FXeCrB3nJ4B9G9DObwb71txY0kHNQ2bqTcpLBbUDW0wrx%2BQHAYSJiDg8uYoFvsSWKYl3Kw%3D%3D";
+  const [list, setList] = useState([]);
   const FetchData = async () => {
     await axios
       .get(
         `http://apis.data.go.kr/6290000/tourdestbaseinfo/gettourdestbaseinfo?serviceKey=${API_KEY}&type=json`
       )
       .then((res) => {
-        setLists(res.data.TourDestBaseInfo);
+        setList(
+          res.data.TourDestBaseInfo.filter((item) => item.id === tour_id)
+        );
       });
   };
 
   useEffect(() => {
     FetchData();
   }, []);
-  const list = lists.filter((item) => item.id === tour_id);
 
-  console.log(tour_id);
   console.log(list);
+
   return (
     <div className="tourInfo">
       <h1 className="LOGO">TRIP</h1>
-      <div className="tourInfoBox"></div>
+      <div className="tourInfoBox">
+        <h1 className="tourDesNm">{list.tourDestNm}</h1>
+        <p>관광지 소개 : {list.tourDestIntro}</p>
+        <p>전화번호 : {list.mngAgcTel}</p>
+        <p>주소 : </p>
+        <p>광관지 구분 : {list.sportsEnterFcltInfo}</p>
+        <p>지정일자 : {list.dsgnDate}</p>
+      </div>
       <Footer />
     </div>
   );
