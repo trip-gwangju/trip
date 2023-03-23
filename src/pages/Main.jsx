5dcfcc6d0075daa49816cbcf8973dd6445b0fff1
이운린 <105215297+likegitman@users.noreply.github.com>
2023-03-23 20:53:26 +0900

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import CityBox from "../components/CityBox";
-import "../styles/Main.css";
+import "../styles/main.css";
 
 function Main() {
   return (
